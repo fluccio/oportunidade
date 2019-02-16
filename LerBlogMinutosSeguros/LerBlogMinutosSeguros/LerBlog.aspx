836173d92074c10657a1fe6095127b4b5f49bad7
fluccio <47690916+fluccio@users.noreply.github.com>
2019-02-16 10:51:40 -0200

    Add files via upload
    Autor: Fernando Luziani Gomes
    Obs: Devido a Problemas de Proxy e Autenticação utilizei o FEED.XML Atualizado do blog Minuto Seguros

---
+++ LerBlogMinutosSeguros/LerBlogMinutosSeguros/LerBlog.aspx
@@ -0,0 +1,79 @@
+﻿<%@ Page Title="Home Page" Language="C#" AutoEventWireup="true"
+    CodeBehind="LerBlog.aspx.cs" Inherits="LerBlogMinutoSeguros._Default" %>
+
+    <head>
+        <style type="text/css">
+            .style1
+            {
+                width: 181px;
+            }
+            .style2
+            {
+                width: 178px;
+            }
+        </style>
+
+
+        <script type="text/javascript" language="javascript">
+            function SomenteNumero(e) {
+                var tecla = (window.event) ? event.keyCode : e.which;
+                if (((tecla > 47 && tecla < 58))) return true;
+                else {
+                    if (tecla != 8) return false;
+                    else return true;
+                }
+            }
+</script>
+
+
+</head>
+<form id="form1" runat="server">
+
+    <h2 style="text-align: center">
+       MINUTO SEGUROS
+    </h2>
+
+    <table style="width:100%;">
+        <tr>
+            <td class="style1">
+                <asp:Label ID="Label1" runat="server" Font-Names="Arial" Font-Size="11pt" 
+                    Text="Quantidade de Caracteres:"></asp:Label>
+            </td>
+            <td class="style2">
+                <asp:TextBox id="txt_QtdCarac" runat="server" 
+                    onkeypress="return SomenteNumero(event)">4</asp:TextBox>
+            </td>
+            <td>
+                <asp:Button ID="btnPesquisa" runat="server" onclick="btnPesquisa_Click" 
+                    Text="Confirmar" />
+            </td>
+        </tr>
+</table>
+<br />
+                <asp:Label ID="Label2" runat="server" Font-Names="Arial" Font-Size="12pt" 
+                    Text="Total Geral" Font-Bold="True" Font-Underline="True"></asp:Label>
+<br />
+<br />
+
+    <asp:Label ID="lblTexto" runat="server" Font-Names="Arial" Font-Size="11pt"></asp:Label>
+
+
+
+<br />
+    <br />
+    <br />
+                <asp:Label ID="Label3" runat="server" Font-Names="Arial" Font-Size="12pt" 
+                    Text="Total Por Tópico" Font-Bold="True" Font-Underline="True"></asp:Label>
+            <br />
+    <br />
+
+    <asp:Label ID="lblTextoPorTopico" runat="server" Font-Names="Arial" Font-Size="11pt"></asp:Label>
+
+
+
+<br />
+</form>
+
+
+
+
---
+++ LerBlogMinutosSeguros/LerBlogMinutosSeguros/LerBlog.aspx.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Net;
+using System.IO;
+using System.Xml;
+using System.Web.Script.Serialization;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Data;
+
+namespace LerBlogMinutoSeguros
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            //DEVIDO A PROBLEMAS DE PROXY E AUTENTICAÇÃO BAIXEI O XML ATUALIZADO DO BLOG
+            // ARQUIVO FEED.XML DENTRO DA PASTA XML
+
+            if (!IsPostBack)
+            {
+                string texto = LeDadosBlog();
+                lblTexto.Text = FormaDados(texto, Convert.ToInt32(txt_QtdCarac.Text));
+
+            }
+
+
+        }
+
+
+        // REALIZA A LEITURA DO BLOG
+        private string LeDadosBlog()
+        {
+            string path = HttpContext.Current.Server.MapPath("~/XML/feed.xml");
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+
+            XmlNode node = doc.GetElementsByTagName("content:encoded").Item(0);
+
+            XmlNodeList elemList = doc.GetElementsByTagName("content:encoded");
+            string texto = "";
+            lblTextoPorTopico.Text = "";
+            for (int i = 0; i <= 9; i++)
+            {
+                string TagPorTopic = "";
+                Regex tagRemove = new Regex(@"<[^>]*(>|$)"); // Removendo a formatação HTML
+                TagPorTopic += tagRemove.Replace(elemList[i].FirstChild.Value, string.Empty);
+                texto += TagPorTopic;
+                lblTextoPorTopico.Text += "Tópico " + (i + 1).ToString() + "<br>" + FormaDados(TagPorTopic, Convert.ToInt32(txt_QtdCarac.Text)) + "<br>";
+            }
+
+            return texto;
+
+        }
+
+
+        // RETIRA AS TAGS HTML / CONTA A QUANTIDADE DE PALAVRAS MAIS USADA E ORDENA
+        private string FormaDados(string texto, int QtdCaracter)
+        {
+
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+
+            texto = texto.Replace(",", "");
+            texto = texto.Replace(".", "");
+            string[] arr = texto.Split(' ');
+
+            foreach (string palavra in arr)
+            {
+                if (palavra.Length >= QtdCaracter) // por padrão na tela deixe 4 caracteres
+                {
+                    if (dictionary.ContainsKey(palavra))
+                        dictionary[palavra] = dictionary[palavra] + 1;
+                    else
+                        dictionary[palavra] = 1;
+                }
+            }
+
+            DataTable dt_Dados = new DataTable("MinutoSeguros");
+            dt_Dados.Columns.Add("Palavra", typeof(string));
+            dt_Dados.Columns.Add("Qtde", typeof(int));
+
+            foreach (KeyValuePair<string, int> palavra in dictionary)
+            {
+
+                dt_Dados.Rows.Add(palavra.Key, Convert.ToInt32(palavra.Value));
+            }
+
+             dt_Dados.DefaultView.Sort = "Qtde desc";
+             dt_Dados = dt_Dados.DefaultView.ToTable();
+
+             texto = "";
+             for (int i = 0; i <= 9; i++)
+             {
+                 texto += (i+1).ToString() + ") "+ "Palavra: " + "<span style='color:red'>" + dt_Dados.Rows[i]["Palavra"].ToString().ToUpper() + "</span>" + " Qtde: " +  "<span style='color:blue'> " + dt_Dados.Rows[i]["Qtde"].ToString()  + "</span>" + "</br>";
+
+             }
+
+
+            return texto;
+
+        }
+
+
+        protected void btnPesquisa_Click(object sender, EventArgs e)
+        {
+
+            string texto = LeDadosBlog();
+            lblTexto.Text = FormaDados(texto, Convert.ToInt32(txt_QtdCarac.Text));
+
+
+        }
+
+
+
+
+
+    }
+}
